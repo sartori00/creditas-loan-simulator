bddcc351520139bcf40b943b8e2612ec93cb68f2
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-09-22 12:47:40 -0300

    first commit

---
+++ src/main/java/br/com/creditas/loansimulator/LoansimulatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.creditas.loansimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansimulatorApplication.class, args);
+	}
+
+}
